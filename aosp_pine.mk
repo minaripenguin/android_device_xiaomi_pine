11345c6ae1406c26cbfbd15590dd135f59ee96f6
dlwlrma123 <alexfinhart@gmail.com>
2022-01-05 15:21:02 +0800

    pine: switch to redfin fp
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- aosp_pine.mk
+++ aosp_pine.mk
@@ -31,7 +31,7 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.211205.016.A1 7957957 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220105.002 7961164 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.211205.016.A1/7957957:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys"
